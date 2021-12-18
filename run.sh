c9d7208cb1b63961edfd6fab0b0fbaa8bed520a6
Gian Erni <gerni@student.ethz.ch>
2021-12-18 12:55:27 +0100

    scheduling

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ module load gcc/8.2.0
 module load python_gpu/3.8.5
 module load eth_proxy
 
-python -m scripts.train_semseg --name segmentation_tiest_100 --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --use_wandb True --num_epochs 100 --batch_size 16
+python -m scripts.train_semseg --name segmentation_test_100_lr --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --use_wandb True --num_epochs 100 --batch_size 16

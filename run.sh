823370bafd8204d37b736206e3807f3fae846ae5
gumbee <mugeeb.hassan@gmail.com>
2021-12-21 21:32:01 +0100

    run baselines

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ module load python_gpu/3.8.5
 module load eth_proxy
 
 # + python -m euler_run_cross_val --name whole_pipeline_test --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False
-# python -m euler_run_baselines --name baselines --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared True
-# python -m euler_run_cross_val --name baselines_ours --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dat/data --use_wandb True --shared False
-python -m euler_run_gam --name gam --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 100
+python -m euler_run_baselines --name baselines --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared True --num_epochs_seg 55 --num_epochs_gogoll 100
+# python -m euler_run_cross_val --name baselines_ours --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dat/data --use_wandb True --shared False --num_epochs_seg 55 --num_epochs_gogoll 100
+# python -m euler_run_gam --name gam --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 100

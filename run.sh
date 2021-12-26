1cd44f679c8ab6d0e360a457d53773e3b63259c5
Gian Erni <gerni@student.ethz.ch>
2021-12-26 17:18:45 +0100

    ready for train gogol

--- run.sh
+++ run.sh
@@ -11,7 +11,20 @@ module load eth_proxy
 # python -m scripts.train_semseg --name trash --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --sched True --num_epochs_seg 1 --batch_size 8
 # python -m scripts.train_semseg --name trash --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --sched False --num_epochs_seg 1 --batch_size 8
 
-python -m scripts.train_semseg --name segmentation_test_16_sched --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --sched True --num_epochs_seg 16 --batch_size 8
-python -m scripts.train_semseg --name segmentation_test_55_sched --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --sched True --num_epochs_seg 55 --batch_size 8
-python -m scripts.train_semseg --name segmentation_test_16_no_sched --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --sched False --num_epochs_seg 16 --batch_size 8
-python -m scripts.train_semseg --name segmentation_test_55_no_sched --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --sched False --num_epochs_seg 55 --batch_size 8
+# python -m scripts.train_semseg --name segmentation_test_16_sched --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --sched True --num_epochs_seg 16 --batch_size 8
+# python -m scripts.train_semseg --name segmentation_test_55_sched --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --sched True --num_epochs_seg 55 --batch_size 8
+# python -m scripts.train_semseg --name segmentation_test_16_no_sched --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --sched False --num_epochs_seg 16 --batch_size 8
+# python -m scripts.train_semseg --name segmentation_test_55_no_sched --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data/source --sched False --num_epochs_seg 55 --batch_size 8
+
+
+python -m scripts.train_gogoll --name gogol_50_r_1 --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 50 --num_epochs_seg 55 --lr_ratio 1
+python -m scripts.train_gogoll --name gogol_100_r_1 --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 100 --num_epochs_seg 55 --lr_ratio 1
+python -m scripts.train_gogoll --name gogol_150_r_1 --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 150 --num_epochs_seg 55 --lr_ratio 1
+
+python -m scripts.train_gogoll --name gogol_50_r_3 --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 50 --num_epochs_seg 55
+python -m scripts.train_gogoll --name gogol_100_r_3 --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 100 --num_epochs_seg 55
+python -m scripts.train_gogoll --name gogol_150_r_3 --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 150 --num_epochs_seg 55
+
+python -m scripts.train_gogoll --name gogol_50_r_9 --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 50 --num_epochs_seg 55 --lr_ratio 9
+python -m scripts.train_gogoll --name gogol_100_r_9 --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 100 --num_epochs_seg 55 --lr_ratio 9
+python -m scripts.train_gogoll --name gogol_150_r_9 --log_dir /cluster/scratch/$USER/logs --dataset_root /cluster/scratch/$USER/dl_data/data --use_wandb True --shared False --num_epochs_gogoll 150 --num_epochs_seg 55 --lr_ratio 9

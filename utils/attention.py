3f8c186361b8f7a205d6e34a4f89c14e343d39a4
milojal1 <aleksandar.milojevic@gmail.com>
2022-01-02 14:28:48 +0100

    remove old utils

--- utils/attention.py
+++ utils/attention.py
@@ -8,15 +8,3 @@ Tensor = torch.cuda.FloatTensor if cudaAvailable else torch.Tensor
 def toZeroThreshold(x, t=0.9):
 	zeros = Tensor(x.shape).fill_(0.0)
 	return torch.where(x > t, x, zeros)
-
-def clamp(x):
-	ones = Tensor(x.float().shape).fill_(1.0)
-	return torch.where(x.float() <= 1.0, x.float(), ones)
-
-def clampToValue(x, value=0.5):
-	values = Tensor(x.float().shape).fill_(value)
-	return torch.where(x.float() == 0.0, x.float(), values)
-
-def extractClass(x, c=2.0):
-	zeros = Tensor(x.float().shape).fill_(0.0)
-	return torch.where(x.float() == c, x.float(), zeros)

fd9afb4ab486789fbd6971be934c1e95292bfcea
Gian Erni <gerni@eu-login-47.euler.ethz.ch>
2022-01-03 12:21:59 +0100

    flexible download

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@ module load gcc/8.2.0
 module load python_gpu/3.8.5
 module load eth_proxy
 
-/bin/bash download_euler.sh /cluster/scratch/$USER/dl_data
-
+if [ ! -d /cluster/scratch/$USER/dl_data ]; then
+  /bin/bash download_euler.sh /cluster/scratch/$USER/dl_data
+fi
 
 # run semseg
 python -m scripts.run_semseg --name semseg --num_epochs_seg 2
